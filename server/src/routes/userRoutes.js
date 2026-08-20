8222d91ee43f9f1a0afa56a01874f8ac0f7873fc
Gautam <gautamshende144@gmail.com>
2026-08-20 15:21:44 +0530

    https status added .

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -2,15 +2,16 @@ import express from 'express'
 import User from '../models/User.js'
 import { protect } from '../middleware/authMiddleware.js'
 import { authorizeRoles } from '../middleware/roleMiddleware.js'
+import { HTTP_STATUS } from '../../constants.js'
 
 const router = express.Router()
 
 router.get('/api/users', protect, authorizeRoles('admin', 'employee'), async (req, res) => {
   try {
     const users = await User.find().select('name email role')
-    res.status(200).json(users)
+    res.status(HTTP_STATUS.OK).json(users)
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ message: error.message })
   }
 })
 
