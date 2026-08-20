c0049165cd4769a0c30fdb4740232a664b6445e5
Gautam <gautamshende144@gmail.com>
2026-08-20 22:26:26 +0530

    server.js minor cors issue fixed.

--- server/server.js
+++ server/server.js
@@ -22,8 +22,6 @@ connectDB()
 
 app.use(cors({
   origin: process.env.FRONTEND_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
